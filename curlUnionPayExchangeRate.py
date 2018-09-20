18144056dcf57bde94907fa88bafdfa7b6288d80
MASER <maser.cuhk@gmail.com>
2018-09-20 16:14:40 +0200

    007 Add Loading csv history data function

--- curlUnionPayExchangeRate.py
+++ curlUnionPayExchangeRate.py
@@ -1,20 +1,33 @@
 import requests
 import json
 import datetime
+import csv
 
+#Server information and post parameter
 url='https://www.unionpayintl.com/cardholderServ/serviceCenter/rate/search'
 data =  {'curDate':'', 'baseCurrency': '', 'transactionCurrency': ''}
 
+#Post command parameters vlaue
 baseCurrency='HKD'
 transactionCurrency='EUR'
 upToDate=True
-startDateObj=datetime.datetime.strptime('2018-08-01', '%Y-%m-%d')
+startDateObj=datetime.datetime.strptime('2018-09-01', '%Y-%m-%d')
 endDateObj=datetime.datetime.strptime('2018-09-15','%Y-%m-%d')
 
 data['baseCurrency']=baseCurrency
 data['transactionCurrency']=transactionCurrency
 history=''
 
+#Load History Data
+with open('HKD-EUR.csv') as historydata:
+	reader = csv.reader(historydata, delimiter=',')
+	historydatalist=list(reader)
+
+#Find String in list
+history_date=[i[0] for i in historydatalist]
+#history_date.index('2018-09-01')
+
+#If Statement for uptoDate
 if upToDate:
 	endDateObj=datetime.datetime.now()
 
