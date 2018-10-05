cccf5620023f95b7b5fac925caf6b96039dc6413
MASER <maser.cuhk@gmail.com>
2018-10-05 16:40:17 +0200

    008 Add Interactive Input Function

--- curlUnionPayExchangeRate.py
+++ curlUnionPayExchangeRate.py
@@ -7,15 +7,31 @@ import csv
 url='https://www.unionpayintl.com/cardholderServ/serviceCenter/rate/search'
 data =  {'curDate':'', 'baseCurrency': '', 'transactionCurrency': ''}
 
-#Post command parameters vlaue
-baseCurrency='HKD'
-transactionCurrency='EUR'
-upToDate=True
-startDateObj=datetime.datetime.strptime('2018-10-01', '%Y-%m-%d')
-endDateObj=datetime.datetime.strptime('2018-09-15','%Y-%m-%d')
+#Input parameters
+data['baseCurrency']= raw_input("Base Currency ( e.g. AUD / CAD / CNY / EUR / GBP / HKD / JPY / MOP / NZD / SGD / THB / USD )\nEnter Base Currency (default:HKD) :").upper() or 'HKD'
+data['transactionCurrency']= raw_input("Transaction Currency ( e.g. USD etc )\nEnter Transaction Currency (default:EUR) :" ).upper() or 'EUR'
 
-data['baseCurrency']=baseCurrency
-data['transactionCurrency']=transactionCurrency
+startDateInput=raw_input("Start Date ( Format:YYYY-MM-DD ) :").upper() or "NOW"
+if startDateInput=="NOW":
+	startDateObj=datetime.datetime.now()
+	endDateObj=datetime.datetime.now()
+else:	
+	endDateInput=raw_input("End Date ( Format:YYYY-MM-DD ) (default:NOW):").upper() or "NOW"
+	if endDateInput=="NOW":
+		try:
+			startDateObj=datetime.datetime.strptime(startDateInput, '%Y-%m-%d')
+		except ValueError as e:
+			exit()
+
+		endDateObj=datetime.datetime.now()
+	else:
+		try:
+			startDateObj=datetime.datetime.strptime(startDateInput, '%Y-%m-%d')
+			endDateObj=datetime.datetime.strptime(endDateInput, '%Y-%m-%d')
+		except ValueError as e:
+			exit()
+
+	
 history=''
 
 #Load History Data
@@ -27,14 +43,11 @@ with open('HKD-EUR.csv') as historydata:
 history_date=[i[0] for i in historydatalist]
 #history_date.index('2018-09-01')
 
-#If Statement for uptoDate
-if upToDate:
-	endDateObj=datetime.datetime.now()
-
 while (startDateObj<=endDateObj):
 	startDateStr=startDateObj.strftime('%Y-%m-%d')
 	data['curDate']=startDateStr
 	response = requests.post(url, data=data)
+	
 	if (len(response.content)!=0):
 		exchangeRate=json.loads(response.content)
 		exchangeRate=str(exchangeRate['exchangeRate'])
@@ -42,6 +55,5 @@ while (startDateObj<=endDateObj):
 		history=exchangeRate
 	else:
 		print (startDateStr+" "+history)
+	
 	startDateObj=startDateObj+datetime.timedelta(days=1)
-
-
